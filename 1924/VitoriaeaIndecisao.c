f38071a54efd2f5c4f84a40c7acd834ece52d40b
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-04-18 23:07:26 -0400

    [ADD] Solution in C for exercise 1924
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ 1924/VitoriaeaIndecisao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int e, i;
+	scanf("%d", &e);
+	char v[e][50];
+	for(i = 0; i <= e; i++) {
+		gets(v[i]);
+	}	
+	printf("Ciencia da Computacao\n");
+	return 0;
+}
