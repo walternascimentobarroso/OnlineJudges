413d070a54a7e67d8827af99c922cbdc47a353c5
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-05-23 21:24:09 -0400

    [ADD] Solution in C for exercise 1005
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

---
+++ 1005/Media1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    double A,B, MEDIA;
+
+	scanf("%lf", &A);
+	scanf("%lf", &B);
+
+	MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+	printf("MEDIA = %.5lf\n", MEDIA);
+	
+    return 0;
+}
