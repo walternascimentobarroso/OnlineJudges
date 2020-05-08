31ca47919fdb5435adb640d6fbeed99c76870395
walternascimentobarroso <walter.nascimento.barroso@gmail.com>
2020-05-08 16:49:06 -0400

    [ADD] 1001 with the js

---
+++ 1001/js/1001.js
@@ -0,0 +1,12 @@
+let path = '/dev/stdin';
+//let path = 'debug.txt';
+let input = require('fs').readFileSync(path, 'utf8');
+let lines = input.split('\n');
+
+//console.log(lines);
+
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+let a = parseInt(lines.shift());
+let b = parseInt(lines.shift());
+   
+console.log('X = ' + (a+b));
