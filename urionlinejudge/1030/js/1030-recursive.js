f2f0c85d5dfbf8cf374d344d9f94fba097108c78
walternascimentobarroso <walter.nascimento.barroso@gmail.com>
2020-05-13 11:41:04 -0400

    :sparkles: Solution 1030

---
+++ urionlinejudge/1030/js/1030-recursive.js
@@ -0,0 +1,17 @@
+// let path = '/dev/stdin';
+ let path = 'debug.txt';
+let input = require('fs').readFileSync(path, 'utf8');
+let lines = input.split('\n');
+
+lines.forEach(main);
+
+function main(line, index) {
+    if (index === 0) return;
+    let n = line.split(' ').map(Number);
+    console.log(`Case ${index}: ${josephus(n[0], n[1])}`);
+
+}
+
+function josephus(a, b) {
+    return (a == 1) ? 1 : (((josephus(a - 1, b) + b - 1) % a) + 1);
+}
---
+++ urionlinejudge/1030/js/1030.js
@@ -0,0 +1,25 @@
+// https://www.youtube.com/watch?v=uCsD3ZGzMgE
+//let path = '/dev/stdin';
+
+let path = 'debug.txt';
+let input = require('fs').readFileSync(path, 'utf8');
+let lines = input.split('\n');
+
+lines.forEach(main);
+
+function main(line, index) {
+    if (index === 0) return;
+    let n = line.split(' ').map(Number);
+    console.log(`Case ${index}: ${josephus(n[0], n[1]-1)}`);
+
+}
+
+function josephus(totalPeople, range) {
+    let array = Array.from(Array(totalPeople), (x, i) => i++);
+    let i = 0;
+    while (array.length > 1) {
+        i = (i + range) % array.length;
+        array.splice(i, 1);
+    }
+    return array[0] + 1;
+}
